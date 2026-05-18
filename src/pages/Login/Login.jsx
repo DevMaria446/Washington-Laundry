0724dad5b838f3965ff5b87bee172fac52f290b6
Great Oletu <bigpanda883@gmail.com>
2026-05-18 10:38:08 +0100

    I design the Sign-up Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,10 @@
-import './login.css';
+import "./login.css";
 const Login = () => {
   return (
-    <div className="auth-container">
-      <h2>Login</h2>
-      <form>
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+<div>
+  
+</div>
   );
-};
+}; 
 
 export default Login;
